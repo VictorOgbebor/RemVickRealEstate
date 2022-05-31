8d3522a8501779d2ccaa99036479acb73f418f00
VictorOgbebor <victor.ogbebor@outlook.com>
2022-05-30 23:56:21 -0500

    added contracts to blockchainFolder.

---
+++ blockchain/contracts/Escrow.sol
@@ -0,0 +1,6 @@
+// this will do all the logic for the rent payment contract
+
+
+// Will Do all the signatures behind the scene and will hold the funds until released
+
+// Reason being We dont want funds on contracts that has alot of activity... best to have a contract specfic to handle money 
